fc18dde6d861bbae1493891d1c47c6a9225b494b
Dennis Öhman <dennis@bjornmman.se>
2020-08-22 22:28:35 +0200

    inital draft

---
+++ src/Our.Umbraco.SimpleElement/App_Plugins/Our.Umbraco.SimpleElement/Js/services.js
@@ -0,0 +1,86 @@
+﻿angular.module('umbraco.services').factory('simpleElementServices', function ($q, contentResource) {
+    var notSupported = [
+        "Umbraco.Tags",
+        "Umbraco.UploadField",
+        "Umbraco.ImageCropper"
+    ];
+
+    var services = {
+        scaffold: function (alias, node) {
+            var defer = $q.defer();
+
+            contentResource.getScaffold(-20, alias).then(function (scaffold) {
+
+                if (!scaffold.isElement) {
+                    defer.reject(alias + " is not element type");
+                    return;
+                }
+
+
+                services.cleanScaffold(scaffold);
+
+                if (node) {
+                    services.populate(scaffold, node);
+                }
+
+                defer.resolve({ scaffold, node });
+
+            }, function (error) {
+                defer.reject(error);
+            });
+
+            return defer.promise;
+        },
+
+        cleanScaffold: function (scaffold) {
+            for (var t = 0; t < scaffold.variants[0].tabs.length; t++) {
+                var tab = scaffold.variants[0].tabs[t];
+                for (var p = tab.properties.length - 1; p >= 0; p--) {
+                    var prop = tab.properties[p]
+                    if (notSupported.indexOf(prop.editor) > -1) {
+                        tab.properties.splice(p, 1);
+                    }
+                }
+            }
+
+            return scaffold;
+        },
+
+        populate: function (scaffold, node) {
+            for (var t = 0; t < scaffold.variants[0].tabs.length; t++) {
+                var tab = scaffold.variants[0].tabs[t];
+                for (var p = 0; p < tab.properties.length; p++) {
+                    var prop = tab.properties[p];
+
+                    if (node[prop.alias]) {
+                        prop.value = node[prop.alias];
+                    } else {
+                        node[prop.alias] = prop.value;
+                    }
+                }
+            }
+        },
+
+        flatten: function (editorNode) {
+            var value = {};
+
+            for (var t = 0; t < editorNode.variants[0].tabs.length; t++) {
+
+                var tab = editorNode.variants[0].tabs[t];
+
+                for (var p = 0; p < tab.properties.length; p++) {
+
+                    var prop = tab.properties[p];
+
+                    if (typeof prop.value !== "function") {
+                        value[prop.alias] = prop.value;
+                    }
+                }
+            }
+
+            return value;
+        }
+    }
+
+    return services;
+});
---
+++ src/Our.Umbraco.SimpleElement/App_Plugins/Our.Umbraco.SimpleElement/Js/simpleelementeditor.controller.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    "use strict";
+    function SimpleElementEditorController($scope, $http, editorState, simpleElementServices) {
+
+        var doctype = $scope.model.config.doctype;
+        var vm = this;
+
+        vm.loading = true;
+        vm.edit = false;
+
+        vm.tabs = [];
+
+
+        vm.changeTab = function(selectedTab) {
+            vm.tabs.forEach(function (tab) {
+                tab.active = false;
+            });
+
+            selectedTab.active = true;
+
+            vm.tab = selectedTab;
+        };
+
+        function init() {
+            vm.loading = true;
+
+            simpleElementServices.scaffold(doctype, $scope.model.data).then(function (result) {
+
+                vm.edit = true;
+
+                vm.tabs = result.scaffold.variants[0].tabs;
+                vm.data = result.menuNode;
+
+                
+                vm.tab = vm.tabs[0];
+
+                vm.loading = false;
+
+            }, function (error) {
+                notificationsService.error("Error loading document type: \"" + $scope.model.selectedDoctype + "\"", error.errorMsg);
+            });
+        }
+
+        init();
+    }
+
+    angular.module("umbraco").controller("SimpleElement.EditorController", SimpleElementEditorController);
+})();
